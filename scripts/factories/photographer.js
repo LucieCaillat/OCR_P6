56b928b6f0022b09ea719f80c55b227a4ff27883
Lucie Caillat <lucie.caillat@gmail.com>
2022-09-06 10:15:39 +0200

    Integrate the homepage

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,46 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    /*const picture = `assets/photographers/${portrait}`;
+    const location = `${city}, ${country}`;
+    const pricePerDay =`${price}€/jour`;*/
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        
+        /*const img = document.createElement( 'img' );
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `image de profil de ${name}`);
+
+        
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        
+        const domLocation = document.createElement( 'p' );
+        domLocation.textContent = location;
+        domLocation.classList.add("location");
+
+        const domTagline = document.createElement( 'p' );
+        domTagline.textContent = tagline;
+        domTagline.classList.add("tagline");
+
+        const domPrice = document.createElement( 'p' );
+        domPrice.textContent = pricePerDay;
+        domPrice.classList.add("price");
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(domLocation);
+        article.appendChild(domTagline);
+        article.appendChild(domPrice);*/
+
+        article.innerHTML = `<img src="assets/photographers/${portrait}" alt="image de profil de ${name}">
+        <h2>${name}</h2>
+        <p class="location">${city}, ${country}</p>
+        <p class="tagline">${tagline}</p>
+        <p class="price">${price}€/jour</p>`;
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
-}
+    return { name, portrait, city, country, tagline, price, getUserCardDOM };
+};
