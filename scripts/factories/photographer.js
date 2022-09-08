a59442097750d498650d7936300eb97850d22490
Lucie Caillat <lucie.caillat@gmail.com>
2022-09-08 17:02:44 +0200

    add PhotographerHeader

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,38 +1,8 @@
 function photographerFactory(data) {
     const { name, portrait, city, country, tagline, price, id } = data;
 
-    /*const picture = `assets/photographers/${portrait}`;
-    const location = `${city}, ${country}`;
-    const pricePerDay =`${price}€/jour`;*/
-
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        
-        /*const img = document.createElement( 'img' );
-        img.setAttribute("src", picture);
-        img.setAttribute("alt", `image de profil de ${name}`);
-
-        
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        
-        const domLocation = document.createElement( 'p' );
-        domLocation.textContent = location;
-        domLocation.classList.add("location");
-
-        const domTagline = document.createElement( 'p' );
-        domTagline.textContent = tagline;
-        domTagline.classList.add("tagline");
-
-        const domPrice = document.createElement( 'p' );
-        domPrice.textContent = pricePerDay;
-        domPrice.classList.add("price");
-
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(domLocation);
-        article.appendChild(domTagline);
-        article.appendChild(domPrice);*/
 
         article.innerHTML = `<a href = "photographer.html?id=${id}"><img src="assets/photographers/${portrait}" alt="image de profil de ${name}"></a>
         <h2><a href = "photographer.html?id=${id}">${name}</a></h2>
@@ -42,5 +12,27 @@ function photographerFactory(data) {
 
         return (article);
     }
-    return { name, portrait, city, country, tagline, price, id, getUserCardDOM };
+
+    function getPhotographerHeaderDOM() {
+        const photographHeader = document.createElement( 'div' );
+        photographHeader.classList.add("photograph-header"); 
+
+
+        photographHeader.innerHTML = 
+        `<div>
+            <h1>${name}</h2>
+            <p class = "location">${city}, ${country}</p>
+            <p class = "tagline">${tagline}</p>
+        </div>
+        <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
+        <img src="assets/photographers/${portrait}" alt="image de profil de ${name}">
+        <div class = "fix-box">
+            <p>nbr of like</p>
+            <p class = "price">${price}€/jour</p>
+        </div>`;
+
+        return (photographHeader);
+    }
+
+    return { name, portrait, city, country, tagline, price, id, getUserCardDOM, getPhotographerHeaderDOM };
 };
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,20 +1,19 @@
 const photographerId = parseInt(new URL(document.location).searchParams.get(`id`)); 
 
 async function getData() {
-  const data = fetch("data/photographers.json")
-      .then(function(res) {
-          return res.json();
-      })
-      .then(function(value) {
-          return value; 
-      });
-  return data;
+  return fetch("data/photographers.json")
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(value) {
+        return value; 
+    });  
 }
 async function getPhotographerData(){
   const data = await getData();
   const photographersDataArray = data.photographers;
   const photographerDataObject = photographersDataArray.filter(phographe => phographe.id === photographerId)[0];
-  console.log(photographerDataObject)
+  
   return photographerDataObject;
 };
 
@@ -22,11 +21,23 @@ async function getPhotosData(){
   const data = await getData();
   const mediaData = data.media;
   const photosDataArray = mediaData.filter(photo => photo.photographerId === photographerId);
-  console.log(photosDataArray)
+  
   return photosDataArray;
 };
 
 
-console.log(photographerId);
-getPhotographerData();
-getPhotosData();
+async function displayData(photographer) {
+  const photographersSection = document.querySelector("#main");
+  const photographerModel = photographerFactory(photographer);
+  const userCardDOM = photographerModel.getPhotographerHeaderDOM();
+  photographersSection.appendChild(userCardDOM);
+};
+
+async function init() {
+  // Recovers data from photographers
+  const photographer = await getPhotographerData();
+  displayData(photographer);
+};
+
+init();
+
